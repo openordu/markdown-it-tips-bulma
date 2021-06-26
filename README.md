bf4159b208913a0f1fbb124b29ba8c27e539ad72
zlluGitHub <18230086651@163.com>
2021-06-26 20:54:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# markdown-it-tips
+Markdown-it plugin to create tips. You can use any type of tip you want, proposed is to use the same type for everything; `warning`,`error`,`success`,`info`,`danger` or keep the type blank for the default styling of a message.
+ 
+```md
+::: warning
+Hello world! [Link](#).
+:::
+```
+
+Gets converted to:
+
+```html
+<div class="md-tip md-tip-warning" role="md-tip">
+<p>Hello world! <a href="#" class="md-tip-link">Link</a>.</p>
+</div>
+```
+
+## Install
+
+```bash
+$ npm install markdown-it-tips --save
+```
+
+## Usage
+
+### Enable plugin
+
+```js
+var md = require('markdown-it');
+var tips = require('markdown-it-tips');
+
+md().use(tips);
+```
+
+
+### Options
+
+Enable/disable adding class `md-tip-link` to links inside tips.
+
+```js
+var md = require('markdown-it');
+var tips = require('markdown-it-tips');
+
+md().use(tips, {links: false});
+```
+
+
+### Example
+
+With option `links` enabled (by default):
+
+```md
+This is a test. [Link](#).
+
+::: success
+Hello world! [Link](#).
+:::
+
+This is a test. [Link](#).
+```
+
+Gets converted to:
+
+```html
+<p>This is a test. <a href="#">Link</a>.</p>
+<p class="md-tip md-tip-success" role="md-tip">
+<p>Hello world! <a href="#" class="md-tip-link">Link</a>.</p>
+</p>
+<p>This is a test. <a href="#">Link</a>.</p>
+```
+
+
+[markdown-it]: https://github.com/markdown-it/markdown-it
+[bootstrap-tips]: http://getbootstrap.com/components/#tips
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var container = require('markdown-it-container');
+
+module.exports = function md_tip_plugin(md, options) {
+	var containerOpenCount = 0;
+	var links = options ? options.links : true;
+	init();
+	return;
+	
+	function setupContainer(name) { 
+        md.use(container, name, {
+            render: function (tokens, idx) {
+                if (tokens[idx].nesting === 1) {
+                    containerOpenCount += 1;
+                    return '<div class="md-tip md-tip-' + name + '">\n';
+                } else {
+                    containerOpenCount -= 1;
+                    return '</div>\n';
+                }
+            }
+        });
+    }
+	
+    function isContainerOpen() {
+        return containerOpenCount > 0;
+    }
+	
+    function selfRender(tokens, idx, options, env, self) {
+        return self.renderToken(tokens, idx, options);
+    }
+    
+	function setupLinks() {
+		var defaultRender = md.renderer.rules.link_open || selfRender;
+		
+		md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+			if (isContainerOpen()) {
+				tokens[idx].attrPush(['class', 'md-tip-link']);
+			}
+			
+			return defaultRender(tokens, idx, options, env, self);
+		};
+	}
+	
+	function init() {
+        setupContainer('success');
+        setupContainer('info');
+        setupContainer('warning');
+        setupContainer('danger');
+        setupContainer('error');
+		
+		if (links) {
+			setupLinks();
+		}
+	}
+};
